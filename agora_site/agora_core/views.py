fb296dd887907fc409ae9a0cb0c0917027245097
Eduardo Robles Elvira <edulix@wadobo.com>
2012-05-01 15:29:22 +0200

    initial home page

--- agora_site/agora_core/views.py
+++ agora_site/agora_core/views.py
@@ -83,7 +83,7 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        #context['activity'] = model_stream(agora)
+        context['activity'] = Action.objects.public()[:2]
         return context
 
 
