285b527585759f00a2fdd4ed55e215e39ee24d29
Eduardo Robles Elvira <edulix@wadobo.com>
2012-09-15 21:03:59 +0200

    correct filtering of agoras in user view

--- agora_site/agora_core/ajax.py
+++ agora_site/agora_core/ajax.py
@@ -6,6 +6,7 @@ from dajaxice.decorators import dajaxice_register
 from agora_site.agora_core.models import Agora, CastVote
 from django.contrib.auth.models import User
 
+from django.shortcuts import get_object_or_404
 from django.utils.simplejson import dumps, loads, JSONEncoder
 from django.core.urlresolvers import reverse
 from django.db.models.query import QuerySet
@@ -17,7 +18,7 @@ def preJson(obj):
     """
         Pre-processor to prepare objects that simplejson cannot serialize
     """
-    
+
     if isinstance(obj, QuerySet) or isinstance(obj, tuple) or isinstance(obj, list):
         return [preJson(i) for i in obj]
     elif isinstance(obj, datetime.date) or isinstance(obj, datetime.datetime):
@@ -33,7 +34,7 @@ def preJson(obj):
         ret = {'username': obj.creator.username,'name': obj.name, 'url': url, 'pretty_name': obj.pretty_name}
         if(obj.result_tallied_at_date != None):
             ret.update({'tallied_date_pretty': pretty_date(obj.result_tallied_at_date), 'tallied_date': preJson(obj.result_tallied_at_date), 'winner': getitem(obj.get_winning_option(), 'value'), 'votes': getitem(obj.get_winning_option(), 'total_count'), 'bar_width': floatformat(getitem(obj.get_winning_option(), 'total_count_percentage'))})
-        
+
         return ret
     else:
         return obj
@@ -43,16 +44,19 @@ def searchAgoras(request, userid, search):
     """
         Ajax endpoint, used from user-view
     """
-    if(len(search) > 0):
-        agoras = Agora.objects.filter(name__startswith=search, creator__id=userid)
+    if userid:
+        user = get_object_or_404(User, pk=userid)
+        agoras = user.agoras
     else:
-        agoras = Agora.objects.all()
-        
-    if(len(agoras) > 0):
-        return dumps({'error': 0, 'data': preJson(agoras)})
+        agoras = Agora.objects
+
+    if(len(search) > 0):
+        agoras = agoras.filter(name__startswith=search, creator__id=userid)
+
+    if(agoras.count() > 0):
+        return dumps({'error': 0, 'data': preJson(agoras.all())})
     else:
         return dumps({'error': 0, 'data': []})
-        
 
 @dajaxice_register
 def searchElectionsForUser(request, userid, search):
@@ -62,20 +66,20 @@ def searchElectionsForUser(request, userid, search):
     users = User.objects.filter(id=userid)
     if(len(users) == 1):
         user = users[0]
-		
+
         ret = []
         for election in user.get_profile().get_participated_elections().filter(pretty_name__startswith=search):
-			vote = user.get_profile().get_vote_in_election(election)
-			pretty_answer = vote.get_chained_first_pretty_answer(election)
-			ret +=  [{'election': election, 'pretty_answer': pretty_answer, 'shown_user': user.username}]
+            vote = user.get_profile().get_vote_in_election(election)
+            pretty_answer = vote.get_chained_first_pretty_answer(election)
+            ret +=  [{'election': election, 'pretty_answer': pretty_answer, 'shown_user': user.username}]
 
         # z = preJson(ret)
         # import pdb; pdb.set_trace()
         return dumps({'error': 0, 'data': preJson(ret)})
     else:
         return dumps({'error': 1, 'message': 'User ' + `userid` + ' not found'})
-		
-		
+
+
 @dajaxice_register
 def searchElectionsForAgora(request, agoraid, search):
     """
@@ -84,14 +88,14 @@ def searchElectionsForAgora(request, agoraid, search):
     agoras = Agora.objects.filter(id=agoraid)
     if(len(agoras) == 1):
         agora = agoras[0]
-        
+
         if(len(search) > 0):
             tmp = agora.get_open_elections_with_name_start(search)
         else:
             tmp = agora.get_open_elections()
 
         elections = [{'date': k, 'pretty_date': pretty_date(k), 'elections': v} for (k,v) in elections_grouped_by_date(tmp).items()]
-        
+
         if(len(elections) > 0):
             return dumps({'error': 0, 'data': preJson(elections)})
         else:
