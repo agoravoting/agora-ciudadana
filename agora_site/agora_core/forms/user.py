1bf767e810fa7e1b75b242e0429962c429879412
Eduardo Robles Elvira <edulix@wadobo.com>
2012-11-29 21:58:37 +0100

    fixing login via API

--- agora_site/agora_core/forms/user.py
+++ agora_site/agora_core/forms/user.py
@@ -1,6 +1,8 @@
 from django import forms as django_forms
 from django.contrib.auth.models import User
-
+from django.contrib.auth import authenticate, login
+from userena import settings as userena_settings
+from userena import forms as userena_forms
 from agora_site.misc.utils import FormRequestMixin
 
 class UsernameAvailableForm(django_forms.Form, FormRequestMixin):
@@ -13,3 +15,21 @@ class UsernameAvailableForm(django_forms.Form, FormRequestMixin):
     class Meta:
         model = User
         fields = ('username')
+
+class LoginForm(userena_forms.AuthenticationForm):
+    def is_valid(self):
+        if super(LoginForm, self).is_valid():
+            identification, password, remember_me = (self.cleaned_data['identification'],
+                self.cleaned_data['password'], self.cleaned_data['remember_me'])
+            user = authenticate(identification=identification, password=password)
+            if user.is_active:
+                login(self.request, user)
+                if remember_me:
+                    self.request.session.set_expiry(userena_settings.USERENA_REMEMBER_ME_DAYS[1] * 86400)
+                else:
+                    self.request.session.set_expiry(0)
+                return True
+            else:
+                return False
+        else:
+            return False
