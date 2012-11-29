fa70abf364304a7a42283a027dfe205081d9fbde
Javier Aguirre <contacto@javaguirre.net>
2012-11-29 18:31:10 +0100

    We can get the properties of the current user via API

--- agora_site/agora_core/resources/user.py
+++ agora_site/agora_core/resources/user.py
@@ -1,3 +1,5 @@
+import json
+
 from django.conf.urls.defaults import *
 from django.contrib.auth.models import User
 from tastypie.utils import trailing_slash
@@ -9,23 +11,27 @@ class UserResource(GenericResource):
     class Meta(GenericMeta):
         queryset = User.objects.all()
         list_allowed_methods = ['get', 'post']
-        detail_allowed_methods = ['get']
+        detail_allowed_methods = ['get', 'put']
+        excludes = ['password', 'is_staff', 'is_superuser']
 
     def override_urls(self):
         return [
-            url(r"^(?P<resource_name>%s)/(?P<username>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+            url(r"^(?P<resource_name>%s)/username/(?P<username>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+            url(r"^(?P<resource_name>%s)/settings%s$" % (self._meta.resource_name, trailing_slash()),
+                                                         self.wrap_view('user_settings'), name="api_user_settings"),
         ]
 
-    def prepend_urls(self):
-        return [
-            url(r"^(?P<resource_name>%s)/register%s$" % (self._meta.resource_name, trailing_slash()),
-                                                         self.wrap_view('register'), name="user_register"),
-            url(r"^(?P<resource_name>%s)/username_available%s$" % (self._meta.resource_name, trailing_slash()),
-                                                                   self.wrap_view('username_available'), name="username_available"),
-            url(r"^(?P<resource_name>%s)/forgot_password%s$" % (self._meta.resource_name, trailing_slash()),
-                                                                self.wrap_view('forgot_password'), name="forgot_password"),
-            url(r"^(?P<resource_name>%s)/reset_password%s$" % (self._meta.resource_name, trailing_slash()),
-                                                               self.wrap_view('reset_password'), name="reset_password"),
-            url(r"^(?P<resource_name>%s)/disable%s$" % (self._meta.resource_name, trailing_slash()),
-                                                         self.wrap_view('user_disable'), name="user_disable"),
-        ]
+    def user_settings(self, request, **kwargs):
+        '''
+            Get the properties of the user currently authenticated
+        '''
+
+        if request.method == 'GET':
+            user = User.objects.get(username=request.user)
+            bundle = self.build_bundle(obj=user, request=request)
+            bundle = self.full_dehydrate(bundle)
+
+            return self.create_response(request, bundle)
+        elif request.method == 'PUT':
+            # TODO
+            pass
