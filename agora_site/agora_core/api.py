8296e670fbcd53a2eb14a6c5b731c64b23901035
Javier Aguirre <contacto@javaguirre.net>
2012-11-29 20:49:24 +0100

    First steps in the actstream API

--- agora_site/agora_core/api.py
+++ agora_site/agora_core/api.py
@@ -3,9 +3,12 @@ from resources.user import UserResource
 from resources.agora import AgoraResource
 from resources.election import ElectionResource
 from resources.castvote import CastVoteResource
+from actstream.resources import FollowResource, ActionResource
 
 v1 = Api("v1")
 v1.register(UserResource())
 v1.register(AgoraResource())
 v1.register(ElectionResource())
 v1.register(CastVoteResource())
+v1.register(FollowResource())
+v1.register(ActionResource())
