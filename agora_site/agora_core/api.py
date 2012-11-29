5a594d679f2d79b4d3d0e733adbcb1f2386855fc
Daniel Garcia Moreno <danigm@wadobo.com>
2012-11-29 15:02:23 +0100

    API: added election resource

--- agora_site/agora_core/api.py
+++ agora_site/agora_core/api.py
@@ -1,7 +1,9 @@
 from tastypie.api import Api
 from resources.user import UserResource
 from resources.agora import AgoraResource
+from resources.election import ElectionResource
 
 v1 = Api("v1")
 v1.register(UserResource())
 v1.register(AgoraResource())
+v1.register(ElectionResource())
