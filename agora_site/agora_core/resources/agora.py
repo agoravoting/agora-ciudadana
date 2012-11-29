b6f374d8b6510a5887ea21e10f9e090fe3a58ecc
Daniel Garcia Moreno <danigm@wadobo.com>
2012-11-29 19:24:21 +0100

    API: Added elections and delegates to Agora

--- agora_site/agora_core/resources/agora.py
+++ agora_site/agora_core/resources/agora.py
@@ -5,10 +5,46 @@ from agora_site.agora_core.resources.user import UserResource
 from tastypie import fields
 
 
+ELECTION_RESOURCE = 'agora_site.agora_core.resources.election.ElectionResource'
+
+
+def open_elections(bundle):
+    # bundle.obj is an Agora
+    return bundle.obj.get_open_elections()
+
+
+def tallied_elections(bundle):
+    return bundle.obj.get_tallied_elections()
+
+
+def all_elections(bundle):
+    return bundle.obj.all_elections()
+
+
+def active_delegates(bundle):
+    return bundle.obj.active_delegates()
+
+
 class AgoraResource(GenericResource):
     creator = fields.ForeignKey(UserResource, 'creator', full=True)
     members = fields.ManyToManyField(UserResource, 'members', full=True)
     admins = fields.ManyToManyField(UserResource, 'admins', full=True)
+
+    open_elections = fields.ToManyField(ELECTION_RESOURCE,
+                                        attribute=open_elections,
+                                        null=True)
+
+    tallied_elections = fields.ToManyField(ELECTION_RESOURCE,
+                                        attribute=tallied_elections,
+                                        null=True)
+
+    all_elections = fields.ToManyField(ELECTION_RESOURCE,
+                                       attribute=all_elections,
+                                       null=True)
+
+    active_delegates = fields.ToManyField(UserResource,
+                                        attribute=active_delegates,
+                                        null=True)
 
     class Meta:
 
