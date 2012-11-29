c9577aa1ed5c4274a1f2c588d2ce1c002ac0bb01
Daniel Garcia Moreno <danigm@wadobo.com>
2012-11-29 21:38:43 +0100

    API: Implemented Agora creation

--- agora_site/agora_core/resources/agora.py
+++ agora_site/agora_core/resources/agora.py
@@ -2,10 +2,41 @@ from agora_site.agora_core.models import Agora
 from agora_site.misc.generic_resource import GenericResource, GenericMeta
 from agora_site.agora_core.resources.user import UserResource
 
+from django.forms import ModelForm
+from django.core.urlresolvers import reverse
+
 from tastypie import fields
+from tastypie.validation import Validation, CleanedDataFormValidation
 
 
 ELECTION_RESOURCE = 'agora_site.agora_core.resources.election.ElectionResource'
+
+
+class CreateAgoraForm(ModelForm):
+    class Meta:
+        model = Agora
+        fields = ('pretty_name', 'short_description', 'is_vote_secret')
+
+
+class AgoraValidation(Validation):
+    def is_valid(self, bundle, request=None):
+        if not bundle.data:
+            return {'__all__': 'Not quite what I had in mind.'}
+
+        if request.method == "POST":
+            return self.validate_post(bundle, request)
+        elif request.method == "PUT":
+            return self.validate_put(bundle, request)
+
+        return {}
+
+    def validate_put(self, bundle, request):
+        # TODO, validate PUT data
+        return {}
+
+    def validate_post(self, bundle, request):
+        form = CleanedDataFormValidation(form_class=CreateAgoraForm)
+        return form.is_valid(bundle, request)
 
 
 def open_elections(bundle):
@@ -46,9 +77,31 @@ class AgoraResource(GenericResource):
                                         attribute=active_delegates,
                                         null=True)
 
-    class Meta:
-
+    class Meta(GenericMeta):
         queryset = Agora.objects.all()
-        #authentication = SessionAuthentication()
         list_allowed_methods = ['get', 'post']
-        detail_allowed_methods = ['get']
+        detail_allowed_methods = ['get', 'post']
+        validation = AgoraValidation()
+
+    def obj_create(self, bundle, request=None, **kwargs):
+        pretty_name = bundle.data['pretty_name']
+        short_description = bundle.data['short_description']
+        is_vote_secret = bundle.data['is_vote_secret']
+
+        agora = Agora(pretty_name=pretty_name,
+                      short_description=short_description,
+                      is_vote_secret=is_vote_secret)
+        agora.create_name(request.user)
+        agora.creator = request.user
+        agora.url = request.build_absolute_uri(reverse('agora-view',
+            kwargs=dict(username=agora.creator.username, agoraname=agora.name)))
+
+        # we need to save before add members
+        agora.save()
+
+        agora.members.add(request.user)
+        agora.admins.add(request.user)
+
+        bundle = self.build_bundle(obj=agora, request=request)
+        bundle = self.full_dehydrate(bundle)
+        return bundle
