7ea89dc6afdacb18ce9d4e1e5e7452bed7d29477
Eduardo Robles Elvira <edulix@wadobo.com>
2012-11-29 18:46:40 +0100

    fixing invalid syntax

--- agora_site/agora_core/resources/user.py
+++ agora_site/agora_core/resources/user.py
@@ -20,11 +20,9 @@ class UserResource(GenericResource):
 
     def override_urls(self):
         return [
-<<<<<<< HEAD
             url(r"^(?P<resource_name>%s)/username/(?P<username>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
             url(r"^(?P<resource_name>%s)/settings%s$" % (self._meta.resource_name, trailing_slash()),
                                                          self.wrap_view('user_settings'), name="api_user_settings"),
-=======
             #url(r"^(?P<resource_name>%s)/register%s$" % (self._meta.resource_name, trailing_slash()),
                                                          #self.wrap_view('register'), name="api_user_register"),
             url(r"^(?P<resource_name>%s)/username_available%s$" \
@@ -38,7 +36,6 @@ class UserResource(GenericResource):
                                                                #name="api_reset_password"),
             #url(r"^(?P<resource_name>%s)/disable%s$" % (self._meta.resource_name, trailing_slash()),
                                                          #self.wrap_view('user_disable'), name="api_user_disable"),
->>>>>>> adding username checking
         ]
 
     def user_settings(self, request, **kwargs):
