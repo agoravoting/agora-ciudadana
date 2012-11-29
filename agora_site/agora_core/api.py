498a00f5befc50132f6e1258a7c2a4b1db9365c8
Daniel Garcia Moreno <danigm@wadobo.com>
2012-11-29 13:59:35 +0100

    API: Added agora initial resource.

--- agora_site/agora_core/api.py
+++ agora_site/agora_core/api.py
@@ -1,5 +1,7 @@
 from tastypie.api import Api
 from resources.user import UserResource
+from resources.agora import AgoraResource
 
 v1 = Api("v1")
 v1.register(UserResource())
+v1.register(AgoraResource())
---
+++ agora_site/agora_core/resources/agora.py
@@ -0,0 +1,16 @@
+from agora_site.agora_core.models import Agora
+from agora_site.misc.generic_resource import GenericResource, GenericMeta
+from agora_site.agora_core.resources.user import UserResource
+
+from tastypie import fields
+
+
+class AgoraResource(GenericResource):
+    creator = fields.ForeignKey(UserResource, 'creator')
+
+    class Meta:
+
+        queryset = Agora.objects.all()
+        #authentication = SessionAuthentication()
+        list_allowed_methods = ['get', 'post']
+        detail_allowed_methods = ['get']
