976f7e99261f3f4936e16c5e673c4f53fa817f47
Eduardo Robles Elvira <edulix@wadobo.com>
2012-12-16 17:34:25 +0100

    only show vote content if its public

--- agora_site/agora_core/resources/castvote.py
+++ agora_site/agora_core/resources/castvote.py
@@ -12,9 +12,14 @@ class CastVoteResource(GenericResource):
     voter = fields.ForeignKey(UserResource, 'voter', full=True)
     election = fields.ForeignKey('agora_site.agora_core.resources.election.ElectionResource',
                                  'election')
+    public_data = fields.DictField(readonly=True)
 
     class Meta:
         queryset = CastVote.objects.all()
         #authentication = SessionAuthentication()
         list_allowed_methods = ['get', 'post']
         detail_allowed_methods = ['get']
+        excludes = ['data']
+
+    def dehydrate_public_data(self, bundle):
+        return bundle.obj.get_public_data()
