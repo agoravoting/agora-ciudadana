62b14471cb707aa684c510f4a84432d6b6cbef82
Eduardo Robles Elvira <edulix@wadobo.com>
2012-12-20 18:05:05 +0100

    fixing user view

--- agora_site/static/js/agora/views/user_list.js
+++ agora_site/static/js/agora/views/user_list.js
@@ -1,15 +1,15 @@
 (function() {
-    var UserListView = Agora.GenericListView.extend({
+    var UserInfiniteView = Agora.GenericListView.extend({
         el: "#activity-list",
         templateEl: "#template-search-profile-item"
     });
 
-    Agora.AgoraListView = Backbone.View.extend({
+    Agora.UserListView = Backbone.View.extend({
         el: "div.search",
 
         initialize: function() {
             _.bindAll(this);
-            this.infiniteListView = new UserListView();
+            this.infiniteListView = new UserInfiniteView();
         }
     });
 }).call(this);
