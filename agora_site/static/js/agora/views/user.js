f30a442ce07f385a845baadfad4f7c11c2f065fb
Eduardo Robles Elvira <edulix@wadobo.com>
2013-02-14 12:19:12 +0100

    adding client side rendered activity to user_view

--- agora_site/static/js/agora/views/user.js
+++ agora_site/static/js/agora/views/user.js
@@ -2,15 +2,13 @@
     var Agora = this.Agora,
         app = this.app;
 
-    Agora.ElectionView = Backbone.View.extend({
-        el: "div.agora",
+    Agora.UserView = Backbone.View.extend({
+        el: "div.user",
 
         initialize: function() {
             _.bindAll(this);
-            //this.calendarView = new Agora.CalendarWidgetView();
-
             // Only initialize on correct section of page exists.
-            if (this.$("#activity-list").length > 0) {
+            if ($("#activity-list").length > 0) {
                 this.activityListView = new Agora.ActivityListView();
             }
         }
