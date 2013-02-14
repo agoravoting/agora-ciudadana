77452c63296748d31d479a1b217592a82ca7c976
Eduardo Robles Elvira <edulix@wadobo.com>
2013-02-14 12:05:51 +0100

    rendering election activity in client

--- agora_site/static/js/agora/views/election.js
+++ agora_site/static/js/agora/views/election.js
@@ -3,16 +3,12 @@
         app = this.app;
 
     Agora.ElectionView = Backbone.View.extend({
-        el: "div.agora",
+        el: "div.election",
 
         initialize: function() {
             _.bindAll(this);
-            //this.calendarView = new Agora.CalendarWidgetView();
-            console.log("inittt");
-
             // Only initialize on correct section of page exists.
-            if (this.$("#activity-list").length > 0) {
-                console.log("inittt2");
+            if ($("#activity-list").length > 0) {
                 this.activityListView = new Agora.ActivityListView();
             }
         }
