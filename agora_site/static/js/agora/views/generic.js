0e8009b8096e2524c8d119b89c445965fa41d296
Eduardo Robles Elvira <edulix@wadobo.com>
2013-03-10 15:53:50 +0100

    fixing calendar view dates

--- agora_site/static/js/agora/views/generic.js
+++ agora_site/static/js/agora/views/generic.js
@@ -54,7 +54,9 @@
                 this.$(".list-container").empty();
 
                 var data = collection.groupBy(function(item) {
-                    var date = item.get('voting_extended_until_date');
+                    var date = item.get('voting_extended_until_date') ||
+                        item.get('voting_ends_at_date') ||
+                        item.get('voting_starts_at_date');
                     if (date) {
                         return date.split("T")[0];
                     } else {
@@ -74,54 +76,6 @@
             }
         });
     }).call(this);
-
-    /*
-     * Generic view for all infinite scroll lists.
-     * DEPRECATED
-    */
-    Agora.InfiniteScrollListView = Backbone.View.extend({
-        el: "#activity-list",
-
-        events: {
-            "click a.endless_more": "onMoreClicked"
-        },
-
-        initialize: function() {
-            _.bindAll(this);
-            $(window).scroll(this.infiniteScroll);
-        },
-
-        infiniteScroll: function() {
-            var doc = $(document), win = $(window);
-            var margin = this.$el.data('endless-smargin') || 1;
-
-            if ((doc.height() - win.height() - win.scrollTop()) <= margin) {
-                this.$("a.endless_more").click();
-            }
-        },
-
-        onMoreClicked: function(event) {
-            console.log("onMoreClicked");
-            event.preventDefault();
-
-            var target = $(event.currentTarget).hide();
-            target.closest('.endless_container').find('.endless_loading').show();
-
-            var data = "querystring_key=" + target.attr('rel').split(' ')[0];
-            var ajax = new Ajax();
-
-            ajax.on("success", this.loadMoreSuccess);
-
-            ajax.setContext({target:target});
-            ajax.get(target.attr('href'), data);
-        },
-
-        loadMoreSuccess: function(xhr, message, ctx) {
-            var data = xhr.responseText;
-            ctx.target.closest('.endless_container').before(data);
-            ctx.target.closest('.endless_container').remove();
-        }
-    });
 
     /*
      * Generic view for all search pages.
