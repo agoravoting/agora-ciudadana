57696a5f81d177868ddba2647be723be24da258b
Eduardo Robles Elvira <edulix@wadobo.com>
2013-04-27 12:27:11 +0200

    working in the new election form

--- agora_site/static/js/agora/views/election_form.js
+++ agora_site/static/js/agora/views/election_form.js
@@ -7,15 +7,49 @@
         tagName: 'div',
 
         className: 'tab-pane',
+        events: {
+            'click .dropdown-menu li a': 'selectVotingSystem',
+        },
 
         id: function() {
             return "question-tab-" + this.model.get('question_num');
         },
 
+        selectVotingSystem: function(e) {
+            var id = $(e.target).closest("li").data('id');
+            var usertext = $(e.target).closest("li").data('usertext');
+            $("#votingsystem").data('id', id);
+            $("#votingsystem").html(usertext);
+            if (id == "MEEK-STV") {
+                $("#num_winners_opts").css('display', 'inline-block');
+            } else {
+                $("#num_winners_opts").hide();
+            }
+            this.model.set('tally_type', id);
+        },
+
         initialize: function() {
             this.template = _.template($("#template-election_form_question_tab_pane").html());
-            this.$el.html(this.template(this.model.toJSON()));
-        }
+
+            var json = this.model.toJSON();
+            this.$el.html(this.template(json));
+
+            // init voting system combo box
+            var self = this;
+            this.$el.find("#votingsystem-dropdown li").each(function () {
+                var id = $(this).data("id");
+                if (id == self.$el.find("#votingsystem").data('id')) {
+                    var usertext = $(this).data("usertext");
+                    self.$el.find("#votingsystem").html(usertext);
+                    if (id == "MEEK-STV") {
+                        self.$el.find("#num_winners_opts").css('display', 'inline-block');
+                    } else {
+                        self.$el.find("#num_winners_opts").hide();
+                    }
+                }
+            });
+            return this.$el;
+        },
     });
 
     Agora.ElectionCreateForm = Backbone.View.extend({
@@ -37,6 +71,7 @@
         events: {
             'click #schedule_voting': 'toggleScheduleVoting',
             'click #show_add_question_tab_btn': 'showAddQuestionTab',
+            'click #schedule_voting_label': 'checkSchedule'
         },
 
         getInitModel: function() {
@@ -44,7 +79,7 @@
                 'pretty_name': '',
                 'description': '',
                 'is_vote_secret': true,
-                'questions': []
+                'questions': [],
             };
         },
 
@@ -60,7 +95,29 @@
 
             // add question tab-content
             var add_question_view = new Agora.QuestionEditView({
-                model: new Agora.QuestionModel({question_num: 0})});
+                model: new Agora.QuestionModel({
+                    'a': 'ballot/question',
+                    'tally_type': 'ONE_CHOICE',
+                    'question_num': 0,
+                    'max': 1,
+                    'min': 0,
+                    'question': 'Do you prefer foo or bar?',
+                    'randomize_answer_order': true,
+                    'answers': [
+                        {
+                            'a': 'ballot/answer',
+                            'url': '',
+                            'details': '',
+                            'value': 'fo\"o'
+                        },
+                        {
+                            'a': 'ballot/answer',
+                            'url': '',
+                            'details': '',
+                            'value': 'bar'
+                        }
+                    ]
+                })});
             this.$el.find('.tab-content').append(add_question_view.render().el);
 
             // populate questions
@@ -81,19 +138,25 @@
             $("#create_election_form").submit(this.createElection);
         },
 
+        checkSchedule: function(e) {
+            if ($("#schedule_voting:checked").length == 0) {
+                $(e.target).closest(".error").removeClass('error');
+            }
+        },
+
         checkStartDate: function() {
-            if (!$("#schedule_voting:checked").length == 0) {
+            if ($("#schedule_voting:checked").length == 0) {
                 return true;
             }
             var val = this.$el.find("#start_voting_date").val();
             if (!val) {
-                return true;
+                return false;
             }
             return isFinite(new Date(val));
         },
 
         checkEndDate: function() {
-            if (!$("#schedule_voting:checked").length == 0) {
+            if ($("#schedule_voting:checked").length == 0) {
                 return true;
             }
             var val = this.$el.find("#end_voting_date").val();
@@ -127,8 +190,8 @@
 
         updateButtonsShown: function() {
             if (this.questionsCollection.length == 0) {
-//                 this.$el.find("#create_election_btn").hide();
-//                 this.$el.find("#show_add_question_tab_btn").show();
+                this.$el.find("#create_election_btn").hide();
+                this.$el.find("#show_add_question_tab_btn").show();
             } else {
                 this.$el.find("#create_election_btn").show();
                 this.$el.find("show_add_question_tab_btn").hide();
