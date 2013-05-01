84502d6c9b91c92817ab96ed170ef8ee287d0cd7
Eduardo Robles Elvira <edulix@wadobo.com>
2013-05-02 00:52:08 +0200

    voting booth: moving start screen to its own view

--- agora_site/static/js/agora/views/voting_booth.js
+++ agora_site/static/js/agora/views/voting_booth.js
@@ -1,5 +1,27 @@
 (function() {
 
+    Agora.VotingBoothStartScreen = Backbone.View.extend({
+        initialize: function() {
+            _.bindAll(this);
+            this.template = _.template($("#template-voting_booth_start_screen").html());
+            this.render();
+
+            return this.$el;
+        },
+
+        render: function() {
+            // render template
+            this.$el.html(this.template(this.model.toJSON()));
+            var converter = new Showdown.converter();
+            var self = this;
+            this.$el.find('.markdown-readonly').each(function (index) {
+                var data_id = $(this).data('id');
+                $(this).html(converter.makeHtml(self.model.get(data_id)));
+            });
+            return this;
+        },
+    });
+
     Agora.VotingBooth = Backbone.View.extend({
         el: "#voting_booth",
 
@@ -17,12 +39,10 @@
         render: function() {
             // render template
             this.$el.html(this.template(this.model.toJSON()));
-            var converter = new Showdown.converter();
-            var self = this;
-            this.$el.find('.markdown-readonly').each(function (index) {
-                var data_id = $(this).data('id');
-                $(this).html(converter.makeHtml(self.model.get(data_id)));
-            });
+
+            this.startScreenView = new Agora.VotingBoothStartScreen({model: this.model});
+            this.$el.find(".start-screen").append(this.startScreenView.render().el);
+
             return this;
         },
     });
