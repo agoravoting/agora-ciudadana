a0057147cd4616cae61690d61495b917000a6742
Eduardo Robles Elvira <edulix@wadobo.com>
2013-05-13 00:30:45 +0200

    fixing and backbonizing last elections widget in agora view

--- agora_site/static/js/agora/views/agora.js
+++ agora_site/static/js/agora/views/agora.js
@@ -2,6 +2,53 @@
     var Agora = this.Agora,
         app = this.app;
 
+    Agora.TalliedElectionsView = Backbone.View.extend({
+        el: "#tallied_elections_view",
+
+        initialize: function() {
+            _.bindAll(this);
+            this.template = _.template($("#template-tallied_elections_view").html());
+            this.render();
+
+            return this.$el;
+        },
+
+        render: function() {
+            // preprocess ajax_data for the template
+
+            // for each eleciton
+            for (var i = 0; i < ajax_data.tallied_elections.objects.length; i++) {
+                var election = ajax_data.tallied_elections.objects[i];
+                election.result.is_simple = false;
+
+                election.result.participation_percentage = 
+                    (election.result.electorate_count * 100.0 / election.result.total_votes);
+                // round two decimals
+                election.result.participation_percentage =
+                    Math.round(election.result.participation_percentage * 100) / 100;
+
+                // in simple elections, find the winner data
+                if (election.result.counts.length == 1 &&
+                    election.result.counts[0].a == "question/result/ONE_CHOICE") {
+                    var winner = election.result.counts[0].winners[0];
+                    var answers = election.result.counts[0].answers;
+                    for (var j = 0; j < answers.length; j++) {
+                        if (answers[j].value == winner) {
+                            election.result.winner_data = answers[j];
+                            election.result.is_simple = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // render template
+            this.$el.html(this.template(ajax_data.tallied_elections));
+            this.delegateEvents();
+            return this;
+        },
+    });
+
     Agora.AgoraView = Backbone.View.extend({
         el: "div.agora",
 
@@ -13,6 +60,7 @@
             // Only initialize on correct section of page exists.
             if ($("#activity-list").length > 0) {
                 this.activityListView = new Agora.ActivityListView();
+                this.talliedElectionsView = new Agora.TalliedElectionsView();
             }
         }
     });
