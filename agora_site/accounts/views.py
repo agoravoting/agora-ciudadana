8ee06bc869af52fb138c904bf61fc2e4446cd810
Eduardo Robles Elvira <edulix@wadobo.com>
2013-05-15 19:02:39 +0200

    fixing some i18n strings

--- agora_site/accounts/views.py
+++ agora_site/accounts/views.py
@@ -28,22 +28,22 @@ from django.views.generic import ListView
 
 class SignUpCompleteView(TemplateView):
     def get(self, request, username):
-        messages.add_message(request, settings.SUCCESS_MODAL, _('Registration ' +
-            'successful! We have sent the activation link to your email ' +
+        messages.add_message(request, settings.SUCCESS_MODAL, _('Registration '
+            'successful! We have sent the activation link to your email '
             'address, look it up.'))
         return redirect('/')
 
 class PasswordResetDoneView(TemplateView):
     def get(self, request):
         messages.add_message(request, settings.SUCCESS_MODAL, _('An e-mail has been '
-            + 'send to you which explains how to reset your password'))
+            'sent to you which explains how to reset your password'))
         return redirect('/')
 
 
 class PasswordResetCompleteView(TemplateView):
     def get(self, request):
         messages.add_message(request, settings.SUCCESS_MODAL, _('Your password has '
-            + 'been reset, you can now <a href="%(url)s">login</a> with your '
-            + 'new password') % dict(url=reverse('userena_signin')))
+            'been reset, you can now <a href="%(url)s">login</a> with your '
+            'new password') % dict(url=reverse('userena_signin')))
         return redirect('/')
 
