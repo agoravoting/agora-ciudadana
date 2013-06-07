fc43f0936155ecde70dcd42361574fa2463d5c54
Daniel Garcia Moreno <danigm@wadobo.com>
2013-06-07 13:48:56 +0200

    Converting plain links in election description

--- agora_site/agora_core/templatetags/string_tags.py
+++ agora_site/agora_core/templatetags/string_tags.py
@@ -1,3 +1,4 @@
+import re
 from django import template
 
 register = template.Library()
@@ -5,3 +6,11 @@ register = template.Library()
 @register.filter
 def truncate_words(string, num_max_words=1):
         return ' '.join(string.split(' ')[:num_max_words])
+
+
+urlfinder = re.compile('^(http:\/\/\S+)')
+urlfinder2 = re.compile('\s(http:\/\/\S+)')
+@register.filter('urlify_markdown')
+def urlify_markdown(value):
+    value = urlfinder.sub(r'<\1>', value)
+    return urlfinder2.sub(r' <\1>', value)
