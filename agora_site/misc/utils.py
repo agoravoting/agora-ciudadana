336904dccd0cd528571d3485ebcb9b58a9e3ef79
Eduardo Robles Elvira <edulix@wadobo.com>
2013-11-12 10:21:23 +0100

    making unit tests work again in django 1.5

--- agora_site/misc/utils.py
+++ agora_site/misc/utils.py
@@ -17,7 +17,7 @@ from django.core.mail import (EmailMultiAlternatives, EmailMessage, send_mail,
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django.db.models import signals
-from django.utils import simplejson as json
+import json
 from django.views.generic import CreateView
 from django.contrib.auth.models import Permission, User
 from django.contrib.contenttypes.models import ContentType
