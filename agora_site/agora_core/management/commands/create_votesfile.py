2239b5a2cf2f57b0d5bc3e2e7f916810961ba368
Eduardo Robles Elvira <edulix@wadobo.com>
2014-02-03 23:36:16 +0100

    improve create_votesfile cmd

--- agora_site/agora_core/management/commands/create_votesfile.py
+++ agora_site/agora_core/management/commands/create_votesfile.py
@@ -53,12 +53,15 @@ class Command(BaseCommand):
     Creates the votes file for an encrypted file
     '''
 
-    args = '<n>'
+    args = '<n> [raw_votes_path]'
     help = 'Adds n test users'
 
     def handle(self, *args, **options):
         try:
+            raw_votes_path = None
             n = int(args[0])
+            if len(args) >= 2:
+                raw_votes_path = args[1]
         except:
             raise CommandError("You need to provide a valid election id")
 
@@ -85,40 +88,93 @@ class Command(BaseCommand):
 
         os.makedirs(os.path.dirname(votes_path))
 
-        with codecs.open(votes_path, encoding='utf-8', mode='w') as votes_file:
-            for vote in election.get_direct_votes():
-                proofs = []
-                choices = []
-                i = 0
-                for question in election.questions:
-                    q_answer = vote.data['answers'][i]
+        raw_file = None
+        def get_votes(election, raw_votes_path):
+            class FakeVote(object):
+                data = None
 
-                    proofs.append(dict(
-                        commitment=q_answer['commitment'],
-                        response=q_answer['response'],
-                        challenge=q_answer['challenge']
-                    ))
-                    choices.append(dict(
-                        alpha=q_answer['alpha'],
-                        beta=q_answer['beta']
-                    ))
-                    i += 1
-                vote_json = dict(
-                    proofs=proofs,
-                    choices=choices
-                )
-                votes_file.write(json.dumps(vote_json) + "\n")
+            if raw_votes_path is None:
+                return election.get_direct_votes()
+            else:
+                raw_file = codecs.open(raw_votes_path, encoding='utf-8', mode='r')
+
+
+        with codecs.open(votes_path, encoding='utf-8', mode='w') as votes_file:
+            # if no file was supplied we read the votes from the db
+            if raw_votes_path is None:
+                for vote in election.get_direct_votes():
+                    proofs = []
+                    choices = []
+                    i = 0
+                    for question in election.questions:
+                        q_answer = vote.data['answers'][i]
+
+                        proofs.append(dict(
+                            commitment=q_answer['commitment'],
+                            response=q_answer['response'],
+                            challenge=q_answer['challenge']
+                        ))
+                        choices.append(dict(
+                            alpha=q_answer['alpha'],
+                            beta=q_answer['beta']
+                        ))
+
+                        i += 1
+                    vote_json = dict(
+                        proofs=proofs,
+                        choices=choices
+                    )
+                    votes_file.write(json.dumps(vote_json) + "\n")
+            # if a file was supplied, read it and parse
+            else:
+                with codecs.open(raw_votes_path, encoding='utf-8', mode='r') as raw_file:
+                    for line in raw_file.readlines():
+                        ballot = json.loads(line)
+                        vote_json = {
+                            "a": "encrypted-vote-v1",
+                            "proofs": [dict(
+                                commitment=ballot['commitment'],
+                                response=ballot['response'],
+                                challenge=ballot['challenge']
+                            )],
+                            "choices": [dict(
+                                alpha=ballot['alpha'],
+                                beta=ballot['beta']
+                            )],
+                            "voter_username": str(uuid.uuid4()),
+                            "issue_date": str(uuid.uuid4()),
+                            "election_hash": {"a": "hash/sha256/value", "value": election.hash},
+                            "election_uuid": election.uuid
+                        }
+                        votes_file.write(json.dumps(vote_json) + "\n")
 
         proto = "https://" if settings.AGORA_USE_HTTPS else "http://"
         votes_url = proto + Site.objects.all()[0].domain + "/media/elections/%d/%s/votes" % (
             election.id, election.orchestra_status['election_id'])
         votes_hash = hash_file(votes_path)
 
+        callback_url = '%s/api/v1/update/election/%d/do_tally/' %\
+            (settings.AGORA_BASE_URL, election.id)
+
         tally_data = {
             "election_id": election.orchestra_status['election_id'],
-            "callback_url": "https://127.0.0.1:5000/public_api/receive_tally",
+            "callback_url": callback_url,
             "extra": [],
             "votes_url": votes_url,
             "votes_hash":"sha512://" + votes_hash
         }
-        print json.dumps(tally_data)
+        print json.dumps(tally_data, indent=4)
+
+        r = requests.post(director.get_public_url('tally'),
+            data=json.dumps(tally_data), verify=False,
+            cert=(settings.SSL_CERT_PATH,
+                settings.SSL_KEY_PATH))
+
+        if r.status_code != 202:
+            status = "error requesting"
+        else:
+            status = 'requested'
+
+        election.orchestra_status['tally_status'] = status
+        election.orchestra_status['tally_director_id'] = director.id
+        election.save()
