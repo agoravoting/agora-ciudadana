1e12fca18cb8d03a12663c8d60e52a1ab6315503
Eduardo Robles Elvira <edulix@wadobo.com>
2014-03-06 21:03:52 +0100

    command to reset the tally of an election

---
+++ agora_site/agora_core/management/commands/resettally.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2014 Eduardo Robles Elvira <edulix AT wadobo DOT com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.core.management.base import BaseCommand, CommandError
+from agora_site.agora_core.models.election import Election
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Resets the tally of an election'
+
+    def handle(self, *args, **options):
+        try:
+            name = args[0]
+        except:
+            raise CommandError("usage: <election.name>")
+
+        e = Election.objects.get(name=name)
+        e.extra_data = None
+        e.voting_ends_at_date = None
+        e.voting_extended_until_date= None
+        e.tally_released_at_date=None
+        e.save()
