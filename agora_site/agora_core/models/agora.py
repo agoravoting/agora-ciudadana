6bb8c9a7b269f63e0cd96c94ee6e37d7c4f6b828
Eduardo Robles Elvira <edulix@wadobo.com>
2014-05-16 14:19:40 +0200

    fix selection of non delegates restrained to an agora

--- agora_site/agora_core/models/agora.py
+++ agora_site/agora_core/models/agora.py
@@ -211,7 +211,7 @@ class Agora(models.Model):
         '''
         from agora_site.agora_core.models import CastVote
 
-        return User.objects.exclude(
+        return self.members.exclude(
             id__in=CastVote.objects.filter(is_counted=True, is_direct=True, is_public=True,
                 invalidated_at_date=None, election__agora__id=self.id).values('voter').query)
 
@@ -223,7 +223,7 @@ class Agora(models.Model):
 
         from agora_site.agora_core.models import CastVote
 
-        return User.objects.filter(
+        return self.members.filter(
             id__in=CastVote.objects.filter(is_counted=True, is_direct=True, is_public=True,
                 invalidated_at_date=None, election__agora__id=self.id).values('voter').query
             )\
